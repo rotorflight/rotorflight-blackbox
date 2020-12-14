5d4ed2a8ee7e9f446020c01a3c769bbd53f890f0
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-12-14 00:01:41 +0000

    RTFL: Add unknown format handling
    Show raw integer value if format is unknown.

--- js/flightlog_fields_presenter.js
+++ js/flightlog_fields_presenter.js
@@ -602,7 +602,7 @@ function FlightLogFieldPresenter() {
             	return FlightLogFieldPresenter.decodeDebugFieldToFriendly(flightLog, fieldName, value, currentFlightMode);
 
             default:
-                return "";
+                return (value).toFixed(0);
         }
     };
     
